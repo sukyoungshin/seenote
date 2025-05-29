8aa39331629bb207ceb9c6f0f731d1cb442d8107
suKyoung <sukyoung.dev@gmail.com>
2025-05-29 12:42:42 +0900

    chore: 전체 파일 Prettier 포맷팅 적용

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,17 @@
-import js from "@eslint/js";
-import tseslint from "typescript-eslint";
-import unusedImports from "eslint-plugin-unused-imports";
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import unusedImports from 'eslint-plugin-unused-imports';
 
 export default [
   js.configs.recommended,
   ...tseslint.configs.recommended,
   {
-    ignores: ["dist", "node_modules", ".next"],
+    ignores: ['dist', 'node_modules', '.next'],
     languageOptions: {
       parser: tseslint.parser,
       parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
+        ecmaVersion: 'latest',
+        sourceType: 'module',
       },
       globals: {
         console: true,
@@ -20,21 +20,21 @@ export default [
       },
     },
     plugins: {
-      "unused-imports": unusedImports,
+      'unused-imports': unusedImports,
     },
     rules: {
-      "unused-imports/no-unused-imports": "warn",
-      "unused-imports/no-unused-vars": [
-        "warn",
+      'unused-imports/no-unused-imports': 'warn',
+      'unused-imports/no-unused-vars': [
+        'warn',
         {
-          vars: "all",
-          varsIgnorePattern: "^_",
-          args: "after-used",
-          argsIgnorePattern: "^_",
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
         },
       ],
-      "no-unused-vars": "off",
-      "no-console": "off",
+      'no-unused-vars': 'off',
+      'no-console': 'off',
     },
   },
 ];
